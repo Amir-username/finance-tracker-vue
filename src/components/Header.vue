c1b43d08159576ba45f1d6b0a73a2c1b8df509bf
Amir <najiamirdesktop@gmail.com>
2025-12-09 22:39:43 +0330

    add header styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,20 +17,27 @@ function handleAddBalanceSubmit() {
   <header>
     <nav>
       <div class="nav-group">
-        <h1>Finance Tracker</h1>
-        <a href="#">dashboard</a>
+        <h1 class="brand">Finance Tracker</h1>
       </div>
       <div class="balance-group">
-        <h3>
-          balance <span> {{ balanceStore.balance }}$ </span>
+        <h3 class="balance">
+          balance
+          <span class="balance-amount"> {{ balanceStore.balance }}$ </span>
         </h3>
         <button
+          class="btn-add-balance"
           v-if="isAddBalanceFormOpen === false"
           @click="isAddBalanceFormOpen = true"
         >
           +
         </button>
-        <button v-else @click="isAddBalanceFormOpen = false">-</button>
+        <button
+          class="btn-remove-balance"
+          v-else
+          @click="isAddBalanceFormOpen = false"
+        >
+          -
+        </button>
 
         <form
           v-show="isAddBalanceFormOpen"
@@ -48,5 +55,42 @@ function handleAddBalanceSubmit() {
 </template>
 
 <style scoped>
+.brand {
+  color: var(--primary);
+  padding: 1rem;
+}
 
+.balance-group {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: aliceblue;
+  padding: 1rem;
+}
+
+.btn-add-balance,
+.btn-remove-balance {
+  border: none;
+  background-color: var(--primary);
+  color: white;
+  border-radius: 100%;
+  width: 32px;
+  height: 32px;
+  font-size: 1rem;
+}
+
+.balance {
+  color: hsl(0, 0%, 46%);
+  text-transform: capitalize;
+}
+
+.balance-amount {
+  font-size: 2rem;
+  color: hsl(0, 0%, 27%);
+}
+
+input {
+  padding: 0.5rem;
+  border: none;
+}
 </style>
